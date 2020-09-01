95615e8bd2c045d5859983c1ab88e9f93531f02f
rodriguezcravero <nacho.rodriguez85@gmail.com>
2020-09-01 16:23:40 -0300

    Updates

---
+++ precache-manifest.bbe4e61b5e41c65a8839a5bbe2737095.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a4c7f9feeac2d092ebbdf70cb04f1d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "256fbc2509499e40239f",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/portfolio/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256fbc2509499e40239f",
+    "url": "/portfolio/static/js/main.10a1b4a7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
